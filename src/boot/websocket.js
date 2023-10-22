c649e4c5312174a7d9650e64765ff17b92ea4dda
Simon Carr <simon@soxprox.com>
2023-10-22 11:49:29 +0000

    add web sockets

--- src/boot/websocket.js
+++ src/boot/websocket.js
@@ -2,15 +2,18 @@
 export default async ({ app, router, store }) => {
   console.log('websocket boot file')
   let ws
-  if (ws) {
+  if (!!ws) {
     ws.close()
   }
-  ws = new WebSocket('ws://localhost:3000/6533c5ed4746ee26e802e650')
+  ws = new WebSocket('ws://localhost:3000/6534f87f0bb69dc839236fff')
   ws.addEventListener('error', (e) => {
     console.log(e)
   })
   ws.addEventListener('message', (e) => {
-    console.log(e.data)
+    const message = JSON.parse(e)
+    const operation = message.operationType
+    const data = message.data
+    console.log(data)
   })
   ws.addEventListener('open', (e) => {
     ws.send('Hello Server!')
